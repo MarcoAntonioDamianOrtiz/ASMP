9b4fc6661e18fca934338150ed278130c28fd873
MarcoAntonioDamianOrtiz <mo27marco@gmail.com>
2025-08-20 20:55:06 -0600

    fix: add logotipo.png for Vercel build

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center">
-        <img src="@/assets/Logotipo.png" alt="Logo UTT" class="h-7 w-auto" />
+        <img src="@/assets/logotipo.png" alt="Logo UTT" class="h-7 w-auto" />
         <span class="text-green-500 text-1xl font-bold">Seguridad Personal</span>
     </div>
 </template>
